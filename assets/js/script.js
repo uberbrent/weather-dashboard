537d951006b9f4eda20f66b111953e89c2103fa7
uberbrent <bmjohns1337@gmail.com>
2020-10-08 12:52:22 -0500

    added second fetch for 5 day forcast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,3 @@
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-        //Parse JSON data to pick the relavent pieces out and place them on the page
 // WHEN I view the UV index
 // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
         //Use an if function to determine the numeral that is received and apply a class corresponding to that number
@@ -20,25 +17,47 @@ var searchBtn = document.querySelector("#searchBtn");
 var listCont = document.querySelector("#prevSearch");
 var cityName = document.querySelector("#cityName");
 var curTemp = document.querySelector("#curTemp")
+var lat = ""
+var lon = ""
 
 var fetchWeather = function(city) {
     if(city === "") {
         alert("Please enter a city!")
         return;
     }
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=7ff4f4e0e52e504f768e877c74b9eab4").then( function(response) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=7ff4f4e0e52e504f768e877c74b9eab4&units=imperial").then( function(response) {
         response.json().then(function (data) {
             showCurWeather(data)
+           lat = data.coord.lat
+           lon = data.coord.lon
+        })
+    })
+    return fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=current,minutely,hourly,alerts&appid=7ff4f4e0e52e504f768e877c74b9eab4&units=imperial")
+    .then(function(response) {
+            response.json().then(function (data) {
+            console.log(data)
         })
     })
 };
 
 var showCurWeather = function (data) { 
-    var actTemp = data.main.temp
-    var tempSplit = actTemp.split(".")[1]
-    cityName.textContent = data.name + " " + date + " " + data.weather[0].icon 
-    
-    curTemp.textContent = "Temperature: " + tempSplit
+    var tempCont = document.createElement("div")
+    tempCont.classList.add("container", "border")
+    var cityName = document.createElement("h2")
+    var temp = document.createElement("p")
+    var humidity = document.createElement("p")
+    var windSpeed = document.createElement("p")
+
+    curTemp.appendChild(tempCont) 
+    tempCont.appendChild(cityName)
+    tempCont.appendChild(temp)
+    tempCont.appendChild(humidity)
+    tempCont.appendChild(windSpeed)
+
+    cityName.textContent = data.name + " (" + date + ") " + data.weather.icon
+    temp.textContent = "Temperature: " + data.main.temp + "° F"
+    humidity.textContent = "Humidity: " + data.main.humidity + " %"
+    windSpeed.textContent = "Wind Speed: " + data.wind.speed + " MPH"
 }
 
 var gatherCity = function() {
