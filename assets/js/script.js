263989b017b9d86c23df8b924113527a01a7a849
uberbrent <bmjohns1337@gmail.com>
2020-10-08 18:00:07 -0500

    completed current weather build, moving on to 5 day forcast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-        //Use an if function to determine the numeral that is received and apply a class corresponding to that number
+
 // WHEN I view future weather conditions for that city
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
 // WHEN I click on a city in the search history
@@ -9,14 +7,13 @@
 
 // 7ff4f4e0e52e504f768e877c74b9eab4 - API KEY
 
-// city.name - city
-//
 var date = moment().format("MM/D/YYYY")
 var searchFieldEl = document.querySelector("#searchField");
 var searchBtn = document.querySelector("#searchBtn");
 var listCont = document.querySelector("#prevSearch");
 var cityName = document.querySelector("#cityName");
-var curTemp = document.querySelector("#curTemp")
+var curTemp = document.querySelector("#curTemp");
+var futureForCont = document.querySelector("#5dayFor");
 var lat = ""
 var lon = ""
 
@@ -30,19 +27,26 @@ var fetchWeather = function(city) {
             showCurWeather(data)
            lat = data.coord.lat
            lon = data.coord.lon
-        })
-    })
-    return fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=current,minutely,hourly,alerts&appid=7ff4f4e0e52e504f768e877c74b9eab4&units=imperial")
-    .then(function(response) {
-            response.json().then(function (data) {
-            console.log(data)
+           getFiveDay();
         })
     })
 };
 
+var getFiveDay = function() {
+    return fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=current,minutely,hourly,alerts&appid=7ff4f4e0e52e504f768e877c74b9eab4&units=imperial")
+    .then(function(response) {
+            response.json().then(function (data) {
+                console.log(data)
+                uvIndexTracker(data)
+
+        })
+    })
+
+}
+
 var showCurWeather = function (data) { 
     var tempCont = document.createElement("div")
-    tempCont.classList.add("container", "border")
+    tempCont.classList.add("container", "border", "currentTemp")
     var cityName = document.createElement("h2")
     var temp = document.createElement("p")
     var humidity = document.createElement("p")
@@ -56,8 +60,36 @@ var showCurWeather = function (data) {
 
     cityName.textContent = data.name + " (" + date + ") " + data.weather.icon
     temp.textContent = "Temperature: " + data.main.temp + "° F"
-    humidity.textContent = "Humidity: " + data.main.humidity + " %"
+    humidity.textContent = "Humidity: " + data.main.humidity + "%"
     windSpeed.textContent = "Wind Speed: " + data.wind.speed + " MPH"
+}
+
+var uvIndexTracker = function(data) {
+    var uvString = parseInt(data.daily[0].uvi)
+    var uvPrint = document.createElement("p")
+    uvPrint.classList.add("uvP")
+    var curTempCont = document.querySelector(".currentTemp")
+    curTempCont.appendChild(uvPrint)
+    uvPrint.textContent = "UV Index: "
+    var uvSpan = document.createElement("span")
+    uvPrint.appendChild(uvSpan)
+    uvSpan.textContent = " " + uvString + " "
+
+    if(uvString < 3) {
+        uvSpan.classList.add("rounded-pill", "bg-success", "text-white")
+    } else if (uvString >= 3 && uvString < 6) {
+        uvSpan.classList.add("rounded-pill", "bg-warning")
+    } else if (uvString >= 5 && uvString < 8) {
+        uvSpan.classList.add("rounded-pill", "bg-orange", "text-white")
+    } else if (uvString >= 8 && uvString < 11) {
+        uvSpan.classList.add("rounded-pill", "bg-danger", "text-white")
+    } else {
+        uvSpan.classList.add("rounded-pill", "bg-purple", "text-white")
+    }
+};
+
+var futureForcast = function(data) {
+    
 }
 
 var gatherCity = function() {
