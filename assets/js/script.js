49e5136eac22a5afe8acc03c0785f3c99b7c5ba5
uberbrent <bmjohns1337@gmail.com>
2020-10-09 15:18:57 -0500

    fixed bugs on adding to list, added localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,7 @@
-
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
-//Have the LI act as clickable search items to get JSON data
 
-// 7ff4f4e0e52e504f768e877c74b9eab4 - API KEY
+var searchHis = []
 
 var date = moment().format("MM/D/YYYY")
 var searchFieldEl = document.querySelector("#searchField");
@@ -24,7 +20,12 @@ var fetchWeather = function (city) {
     }
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=7ff4f4e0e52e504f768e877c74b9eab4&units=imperial").then(function (response) {
         response.json().then(function (data) {
-            showCurWeather(data)
+            if(data.cod === "404") {
+                alert("No city found")
+                return;
+            }
+            buildList(city);
+            showCurWeather(data);
             lat = data.coord.lat
             lon = data.coord.lon
             getFiveDay();
@@ -44,29 +45,30 @@ var getFiveDay = function () {
 }
 
 var showCurWeather = function (data) {
+
     var tempCont = document.createElement("div")
-    tempCont.classList.add("container", "border", "currentTemp")
     var cityName = document.createElement("h2")
-    var imgCont = document.createElement("img")
     var temp = document.createElement("p")
     var humidity = document.createElement("p")
     var windSpeed = document.createElement("p")
 
-    imgCont.setAttribute("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
-    
+    tempCont.classList.add("container", "border", "currentTemp")
+    tempCont.id = "currentContainer"
+
     curTemp.appendChild(tempCont)
     tempCont.appendChild(cityName)
-    cityName.appendChild(imgCont)
     tempCont.appendChild(temp)
     tempCont.appendChild(humidity)
     tempCont.appendChild(windSpeed)
-    
-    
-    
+
     cityName.textContent = data.name + " (" + date + ") "
     temp.textContent = "Temperature: " + data.main.temp + "° F"
     humidity.textContent = "Humidity: " + data.main.humidity + "%"
     windSpeed.textContent = "Wind Speed: " + data.wind.speed + " MPH"
+
+    var theImg = document.createElement("img")
+    cityName.appendChild(theImg)
+    theImg.setAttribute("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
 }
 
 var uvIndexTracker = function (data) {
@@ -85,7 +87,7 @@ var uvIndexTracker = function (data) {
     } else if (uvString >= 3 && uvString < 6) {
         uvSpan.classList.add("rounded", "bg-warning", "p-1")
     } else if (uvString >= 5 && uvString < 8) {
-        uvSpan.classList.add("rounded", "bg-orange", "text-white", "p-1") 
+        uvSpan.classList.add("rounded", "bg-orange", "text-white", "p-1")
     } else if (uvString >= 8 && uvString < 11) {
         uvSpan.classList.add("rounded", "bg-danger", "text-white", "p-1")
     } else {
@@ -97,15 +99,15 @@ var futureForcast = function (data) {
     var fiveDayHeader = document.createElement("h2")
     var cardCont = document.createElement("div")
 
-    fiveDayHeader.classList.add("row")
-    cardCont.classList.add("row", "justify-content-around")
+    fiveDayHeader.classList.add("row", "futureHeader")
+    cardCont.classList.add("row", "justify-content-around", "futureForcast")
 
     futureForEl.appendChild(fiveDayHeader)
     futureForEl.appendChild(cardCont)
 
     fiveDayHeader.textContent = "5-Day Forcast:"
 
-    for(i = 1; i < 6; i++) {
+    for (i = 1; i < 6; i++) {
         var cardCreate = document.createElement("div")
         var cardBody = document.createElement("div")
         var dateP = document.createElement("p")
@@ -129,12 +131,28 @@ var futureForcast = function (data) {
         tempP.textContent = "Temp: " + data.daily[i].temp.day + "° F"
         humidityP.textContent = "Humidity: " + data.daily[i].humidity + "%"
     }
+}
+
+var clickHandler = function () {
+    if (searchBtn) {
+        var citySelection = searchFieldEl.value.trim();
+        gatherCity(citySelection)
+    } else if (listCont) {
+        var citySelection = prevSearch.value.trim();
+        gatherCity(citySelection)
+    }
 
 }
 
-var gatherCity = function () {
-    var citySelection = searchField.value.trim();
-    buildList(citySelection);
+var gatherCity = function (citySelection) {
+    var oldCur = document.querySelector(".currentTemp")
+    var oldHeader = document.querySelector(".futureHeader")
+    var oldFuture = document.querySelector(".futureForcast")
+    if (oldCur) {
+        oldCur.remove();
+        oldHeader.remove();
+        oldFuture.remove();
+    }
     fetchWeather(citySelection);
 };
 
@@ -148,11 +166,28 @@ var buildList = function (city) {
     var cityText = document.createElement("p");
 
     cityText.textContent = searchTerm
-    addCard.classList.add("card");
+    addCard.classList.add("card", "prevSearch");
 
     listItem.appendChild(cityText);
     addCard.appendChild(listItem);
     listCont.appendChild(addCard);
+
+    searchHis.push(city)
+    localStorage.setItem("searchHis", JSON.stringify(searchHis))
 };
 
-searchBtn.addEventListener("click", gatherCity)
+
+var getLocal = function () {
+    var history = JSON.parse(localStorage.getItem("searchHis"))
+    if (history === null) {
+        return;
+    }
+    for (i = 0; i < history.length; i++) {
+        buildList(history[i])
+    }
+}
+
+searchBtn.addEventListener("click", clickHandler)
+listCont.addEventListener("click", clickHandler)
+
+getLocal();
